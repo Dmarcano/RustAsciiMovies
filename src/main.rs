9bac9c38ceed28ea896361945a67819544baf6b5
Dmarcano <dmarcan1@swarthmore.edu>
2020-08-26 03:14:30 -0400

    reading from file

--- src/main.rs
+++ src/main.rs
@@ -3,10 +3,28 @@ mod learn_list;
 
 use learn_list::first_list;
 use crate::movie_reel::play_movie::run;
-
+use std::io;
+use std::io::prelude::*;
+use std::fs::File;
+use std::path::Path;
 
 
 fn main() {
-    let file_name = String::from("public/file2.txt");
-    run(file_name)
+    let path =Path::new("public/file2.txt");
+    let display = path.display();
+
+    // Open the path in read-only mode, returns `io::Result<File>`
+    let mut file = match File::open(&path) {
+        Err(why) => panic!("couldn't open {}: {}", display, why),
+        Ok(file) => file,
+    };
+
+    // Read the file contents into a string, returns `io::Result<usize>`
+    let mut s = String::new();
+    match file.read_to_string(&mut s) {
+        Err(why) => panic!("couldn't read {}: {}", display, why),
+        Ok(_) => print!("{} contains:\n{}", display, s),
+    }
+    // run(file_name)
+
 }
